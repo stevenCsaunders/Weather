9ea49edf039de0dbcb2666ed17f09fd98c586645
Steven Saunders <Steven.Saunders@Steve-Macbook-Pro-MTC.local>
2019-03-13 17:09:58 -0600

    Updated P-List file, updated methods in Weather View Controller, added gps capability and started the calls to the openweather.com API

--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
@@ -21,34 +23,48 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     let WEATHER_URL = "api.openweathermap.org/data/2.5/weather"
     let API_KEY = "377d1530c1b46819e9c975e44fd191a0"
     
-    
     //Instance Variables
-    let locationManager = CLLocationManagerDelegate()
-    
-    
-    
-    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //TO DO: – Add Location Manager Here
-        locationManager.delegate = 
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
     }
     
     
 
     
     
-}
+
 
 
 
 
 //MARK: – Networking
 /*************************************************/
-
+    func getWeatherData (url : String, parameters : [String : String]) {
+        
+        Alamofire.request (url, method : .get, parameters : parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success, got the data")
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                self.currentCityLabel.text = "Connection Issues"
+                
+            }
+        }
+        
+    }
 
 
 
@@ -69,10 +85,34 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 //MARK: – Location Manager Methods
 /*************************************************/
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params = ["lat" : latitude, "lon" : longitude, "appid" : API_KEY]
+            
+            getWeatherData(url : WEATHER_URL, parameters : params)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        currentCityLabel.text = "Location Unavailable"
+    }
 
-
-
+    
+    
 
 
 //MARK: – Change City Delegate Methods
 /*************************************************/
+
+
+
+    }
